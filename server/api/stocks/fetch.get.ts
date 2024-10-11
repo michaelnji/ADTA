fdb2f57eaebde65690cb995de7e6ff910b3058b2
COD3VIL <codebase65@outlook.com>
2024-10-11 23:47:16 +0100

    refactor: ♻️ more improvements to the news component

--- server/api/stocks/fetch.get.ts
+++ server/api/stocks/fetch.get.ts
@@ -1,10 +1,7 @@
-import { format } from 'date-fns'
-import type { CountryData, FxPairsData, Timeseries } from '~/server/types/fx.types'
 import { createClient } from "@supabase/supabase-js";
-import type { News } from '~/server/types/news.types'
-import { sendServerResponse } from '~/server/utils/response'
-import type { Database } from '~/types/database.types'
-import { signJWT } from '~/utils/auth'
+import { sendServerResponse } from '~/server/utils/response';
+import type { Database } from '~/types/database.types';
+import { signJWT } from '~/utils/auth';
 const runtimeConfig = useRuntimeConfig()
 export default eventHandler(async (event) => {
     const token = signJWT(runtimeConfig.supabaseBotId);
@@ -29,7 +26,7 @@ export default eventHandler(async (event) => {
 
     try {
         // get current stocks on database
-        const { data, error } = await client.from("TOP STOCKS LIST WTCHLIST").select('*')
+        const { data, error } = await client.from("TOP STOCKS LIST").select('*')
         if (error) {
             setResponseStatus(event, 500)
             return sendServerResponse(500, error?.message)
